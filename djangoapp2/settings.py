bc3d05f6787b930a87a0fd482983e67ce797466a
kisik <ertw9622@gmail.com>
2021-07-03 11:15:02 +0900

    admin features

--- djangoapp2/settings.py
+++ djangoapp2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'djangoapp2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
